5e84781dc67835d80fc8b461db88ce1782fd9586
bahadirsofuoglu <bahadirsofuoglu@gmail.com>
2021-02-19 01:41:07 +0300

    tree created

--- src/components/Tree.vue
+++ src/components/Tree.vue
@@ -1,3 +1,12 @@
 <template>
-  <h1>hello</h1>
+  <div class="tree">
+    <div class="head h1" />
+    <div class="head h2" />
+    <div class="head h3" />
+    <div class="head h4" />
+    <div class="head h5" />
+    <div class="head h6" />
+    <div class="trunk" />
+    <div class="graund" />
+  </div>
 </template>
