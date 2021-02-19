ebd26a3a7a23837998bf717025c9e16b6e349c30
bahadirsofuoglu <bahadirsofuoglu@gmail.com>
2021-02-20 00:42:31 +0300

    one apple added

---
+++ src/components/Apples.vue
@@ -0,0 +1,3 @@
+<template>
+  <div class="apple"></div>
+</template>
--- src/views/Index.vue
+++ src/views/Index.vue
@@ -2,15 +2,18 @@
   <div class="container">
     <Tree />
     <Basket />
+    <Apples />
   </div>
 </template>
 <script>
 import Tree from '@/components/Tree'
 import Basket from '@/components/Basket'
+import Apples from '@/components/Apples'
 export default {
   components: {
     Tree,
-    Basket
+    Basket,
+    Apples
   }
 }
 </script>
