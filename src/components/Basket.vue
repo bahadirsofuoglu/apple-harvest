61ad6e9634b6d991fa52c5080b7472bdcc04dc9f
bahadirsofuoglu <bahadirsofuoglu@gmail.com>
2021-02-20 00:14:01 +0300

    basket completed

--- src/components/Basket.vue
+++ src/components/Basket.vue
@@ -1,3 +1,12 @@
 <template>
-  <h1>hello</h1>
+  <div class="basket">
+    <div class="basket-header"></div>
+    <div class="body"></div>
+    <div class="line1"></div>
+    <div class="line2"></div>
+    <div class="line3"></div>
+    <div class="line4"></div>
+    <div class="line5"></div>
+    <div class="line6"></div>
+  </div>
 </template>
