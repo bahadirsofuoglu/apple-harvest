b75843af0d2e49cf00bed0c30a7dac8822a0bb7b
bahadirsofuoglu <bahadirsofuoglu@gmail.com>
2021-02-20 20:25:24 +0300

    ground shake problem fixed

--- src/components/Tree.vue
+++ src/components/Tree.vue
@@ -1,12 +1,15 @@
 <template>
-  <div :class="`tree ${shakeClass}`">
-    <div class="head h1" />
-    <div class="head h2" />
-    <div class="head h3" />
-    <div class="head h4" />
-    <div class="head h5" />
-    <div class="head h6" />
-    <div class="trunk" />
+  <div>
+    <div :class="`tree ${shakeClass}`">
+      <div class="head h1" />
+      <div class="head h2" />
+      <div class="head h3" />
+      <div class="head h4" />
+      <div class="head h5" />
+      <div class="head h6" />
+      <div class="trunk" />
+    </div>
+
     <div class="graund" />
   </div>
 </template>
