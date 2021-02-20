b963f00e12168b47bc668c7b07a53a69a0ff1829
bahadirsofuoglu <bahadirsofuoglu@gmail.com>
2021-02-20 20:55:38 +0300

    basket lines deleted

--- src/components/Basket.vue
+++ src/components/Basket.vue
@@ -2,11 +2,5 @@
   <div class="basket">
     <div class="basket-header"></div>
     <div class="body"></div>
-    <div class="line1"></div>
-    <div class="line2"></div>
-    <div class="line3"></div>
-    <div class="line4"></div>
-    <div class="line5"></div>
-    <div class="line6"></div>
   </div>
 </template>
