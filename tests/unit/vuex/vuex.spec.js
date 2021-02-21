fde8f80a577cc62c871c3c5f82eaae86eb454dd3
bahadirsofuoglu <bahadirsofuoglu@gmail.com>
2021-02-21 21:16:18 +0300

    action test completed

--- tests/unit/vuex/vuex.spec.js
+++ tests/unit/vuex/vuex.spec.js
@@ -1,5 +1,5 @@
-import mutation from './vuex/mutation'
-import action from './vuex/action'
+import mutation from './mutation'
+import action from './action'
 
 describe('Vuex testing', () => {
   it('shakeClass-dropedClass changes with CHANGE_CLASS mutation', () => {
@@ -12,5 +12,11 @@ describe('Vuex testing', () => {
     setTimeout(() => {
       expect(state.dropedClass).toBe('-dropped')
     }, 2000)
+  })
+
+  it('Waiting CHANGE_CLASS after action', () => {
+    const commit = jest.fn()
+    action.shakeAndDropApples({ commit })
+    expect(commit).toHaveBeenCalledWith('CHANGE_CLASS')
   })
 })
